88a4c35ff6131ed5ce03532dcc04e7021b8777c6
ChUngHEe0227 <rogo0227@naver.com>
2020-03-17 13:13:36 +0900

    중복 노드삭제에 대한 테스트 코드

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -1,11 +1,12 @@
 import LinkedList.*;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class LinkedListTest {
     Node node1 = new Node(1);
     Node node2 = new Node(2);
-
-    @Test
+    DeletDuplicateExample deletDuplicateExample =new DeletDuplicateExample();
+    @Before
     public void 노드생성(){
         for(int i= 0 ; i<10;i++)
             node1.appendToTail(i);
@@ -15,10 +16,15 @@ public class LinkedListTest {
     }
     @Test
     public void 노드확인(){
-        노드생성();
         assertSame(node1.getData(),1);
         assertSame(node1.getNext().getData(),0);
         assertSame(node2.getData(),2);
         assertSame(node2.getNext().getData(),1);
+    }
+    @Test
+    public void 중복노드삭제확인(){
+        deletDuplicateExample.DeletDuplicate(node2);
+        assertSame(node2.getNext().getData(),1);
+        assertSame(node2.getNext().getNext(),null);
     }
 }
