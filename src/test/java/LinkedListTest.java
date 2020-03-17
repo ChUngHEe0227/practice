86508b01d44173ba02bdc96154cdaaf7ae2c7c98
ChUngHEe0227 <rogo0227@naver.com>
2020-03-17 13:43:52 +0900

    노드에서 뒤에서 N번째 노드의 값 찾기에 대한 test코드

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -6,6 +6,7 @@ public class LinkedListTest {
     Node node1 = new Node(1);
     Node node2 = new Node(2);
     DeletDuplicateExample deletDuplicateExample =new DeletDuplicateExample();
+    Find_the_nth_element_behind_examlpe findTheNthElementBehindExamlpe= new Find_the_nth_element_behind_examlpe();
     @Before
     public void 노드생성(){
         for(int i= 0 ; i<10;i++)
@@ -26,5 +27,10 @@ public class LinkedListTest {
         deletDuplicateExample.DeletDuplicate(node2);
         assertSame(node2.getNext().getData(),1);
         assertSame(node2.getNext().getNext(),null);
+    }
+    @Test
+    public void 끝에서_n번째_노드확인(){
+        assertSame(findTheNthElementBehindExamlpe.Find_the_nth_element_behind(node1,1).getData(),9);
+        assertSame(findTheNthElementBehindExamlpe.Find_the_nth_element_behind(node1,8).getData(),2);
     }
 }
