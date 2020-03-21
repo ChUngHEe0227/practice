027987744ed4e6d0db57d11d45ace4dc44aee4dc
ChUngHEe0227 <rogo0227@naver.com>
2020-03-21 19:50:54 +0900

    스텍에서 가장작은값을 출력하는 함수 작성 O(1)

--- src/main/java/StackAndQueue/MyStack.java
+++ src/main/java/StackAndQueue/MyStack.java
@@ -11,19 +11,36 @@ public class MyStack {
         }
     }
     private StackNode top;
+    private StackNode min;
+
     public char pop(){
         if(top==null)throw new EmptyStackException();
+        char item = top.data;
         top=top.next;
-        return top.data;
+        return item;
     }
     public void push(char item){
         StackNode stackNode = new StackNode(item);
+        while(stackNode.next!=null)
+        {
+            if (stackNode.data > item) {
+                min.next = min;
+                min = stackNode;
+            }
+            stackNode = stackNode.next;
+        }
         stackNode.next=top;
         top = stackNode;
     }
     public char peek(){
         if(top==null) throw new EmptyStackException();
         return top.data;
+    }
+    public char popMin(){
+        if(min==null)throw new EmptyStackException();
+        char item = min.data;
+        min=min.next;
+        return item;
     }
     public boolean isEmpty(){
         return top==null;
