1c27866a09575f738b88c45282a1c4c00a00acf7
ChUngHEe0227 <rogo0227@naver.com>
2020-03-21 21:17:08 +0900

    Min code 제거

--- src/main/java/StackAndQueue/MyStack.java
+++ src/main/java/StackAndQueue/MyStack.java
@@ -21,26 +21,12 @@ public class MyStack {
     }
     public void push(char item){
         StackNode stackNode = new StackNode(item);
-        while(stackNode.next!=null)
-        {
-            if (stackNode.data > item) {
-                min.next = min;
-                min = stackNode;
-            }
-            stackNode = stackNode.next;
-        }
         stackNode.next=top;
         top = stackNode;
     }
     public char peek(){
         if(top==null) throw new EmptyStackException();
         return top.data;
-    }
-    public char popMin(){
-        if(min==null)throw new EmptyStackException();
-        char item = min.data;
-        min=min.next;
-        return item;
     }
     public boolean isEmpty(){
         return top==null;
