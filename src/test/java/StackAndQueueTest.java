9d53479db2bd86117bae4875fe80e7f0f4349b7a
ChUngHEe0227 <rogo0227@naver.com>
2020-03-21 21:18:51 +0900

    미니멈 값을 출력하는 class 의 미니멈 메소드 테스트 코드 작성

--- src/test/java/StackAndQueueTest.java
+++ src/test/java/StackAndQueueTest.java
@@ -1,21 +1,35 @@
 import StackAndQueue.*;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import static org.junit.Assert.*;
-
 public class StackAndQueueTest {
-    MyStack myStack =new MyStack();
-    @Test
+    MyStack MyStack =new MyStack();
+    StackWithMin MinStack =new StackWithMin();
+    @Before
     public void 스텍이잘만들어졌는지확인(){
-        myStack.push('1');
-        myStack.push('9');
-        myStack.push('2');
-        myStack.push('8');
-        myStack.push('3');
-        myStack.push('7');
-        myStack.push('4');
-        myStack.push('6');
-        myStack.push('5');
-        myStack.printStack();
+        MyStack.push('1');
+        MyStack.push('9');
+        MyStack.push('2');
+        MyStack.push('8');
+
+        MinStack.printStack();
+    }
+    @Test
+    public void 미니멈확인(){
+        MinStack.push('0');
+        MinStack.push('6');
+        MinStack.push('4');
+        MinStack.push('7');
+        MinStack.push('3');
+        MinStack.push('8');
+        MinStack.push('2');
+        MinStack.push('9');
+        MinStack.push('1');
+        assertSame('0',MinStack.min());
+        MinStack.pop();
+        assertSame('1',MinStack.min());
+        MinStack.pop();
+        assertSame('2',MinStack.min());
+        MinStack.pop();
+        assertSame('3',MinStack.min());
     }
 }
