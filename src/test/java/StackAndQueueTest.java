5e849bd339db18e2fec3389ed3c59db0b2cca8b7
ChUngHEe0227 <rogo0227@naver.com>
2020-03-22 01:17:58 +0900

    스텍 소팅에 대한 테스트 코드 작성 결과 값이 동일한데 assrt함수에서 false 가 뜸

--- src/test/java/StackAndQueueTest.java
+++ src/test/java/StackAndQueueTest.java
@@ -1,9 +1,13 @@
 import StackAndQueue.*;
 import org.junit.*;
+
+import java.util.Stack;
+
 import static org.junit.Assert.*;
 public class StackAndQueueTest {
     MyStack MyStack =new MyStack();
     StackWithMin MinStack =new StackWithMin();
+    SortStackExample sortStackExample = new SortStackExample();
     @Before
     public void 스텍이잘만들어졌는지확인(){
         MyStack.push('1');
@@ -15,7 +19,7 @@ public class StackAndQueueTest {
     }
     @Test
     public void 미니멈확인(){
-        MinStack.push('0');
+        MinStack.push('5');
         MinStack.push('6');
         MinStack.push('4');
         MinStack.push('7');
@@ -24,12 +28,22 @@ public class StackAndQueueTest {
         MinStack.push('2');
         MinStack.push('9');
         MinStack.push('1');
-        assertSame('0',MinStack.min());
-        MinStack.pop();
         assertSame('1',MinStack.min());
         MinStack.pop();
         assertSame('2',MinStack.min());
         MinStack.pop();
+        assertSame('2',MinStack.min());
+        MinStack.pop();
         assertSame('3',MinStack.min());
+    }
+    @Ignore // 결과 값만 확인
+    public void 스텍소팅(){
+        Stack<Integer> Answerstack= new Stack();
+        Stack<Integer> stack= new Stack();
+        for(int i=0 ; i<=10 ;i++){
+            stack.push(i);
+            Answerstack.push(10-i);
+        }
+        assertSame(stack,sortStackExample.SortStack(Answerstack));
     }
 }
