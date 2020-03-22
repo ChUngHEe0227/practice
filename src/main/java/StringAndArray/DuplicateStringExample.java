24402da4fd447aaa3fb84a7e4d0020410897c985
ChUngHEe0227 <rogo0227@naver.com>
2020-03-22 21:28:08 +0900

    프린트 문 제거

--- src/main/java/StringAndArray/DuplicateStringExample.java
+++ src/main/java/StringAndArray/DuplicateStringExample.java
@@ -9,13 +9,11 @@ public class DuplicateStringExample {
 
     public Boolean CheckDuplicateString(String checkString){
         String stream = new String(checkString);
-        System.out.print(checkString);
         Boolean[] char_value = new Boolean[128];
         Arrays.fill(char_value,false);
         for (int i=0 ;i<stream.length();i++){
             int val = stream.charAt(i);
             if(char_value[val]){
-                System.out.print("Duplicate");
                 return false;
             }
             char_value[val] = true;
