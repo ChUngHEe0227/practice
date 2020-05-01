336064273faf7f6bac6e47c66dee8c5f138cd421
ChUngHEe0227 <rogo0227@naver.com>
2020-05-01 22:17:39 +0900

    격자점이 있을때 최단 거리가 있는지 확인하는 알고리즘

--- src/test/java/RecursiveTest.java
+++ src/test/java/RecursiveTest.java
@@ -1,14 +1,29 @@
+import Recursive.GridRobot;
 import Recursive.Thripstep;
 
 import org.junit.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class RecursiveTest {
     Thripstep thripstep = new Thripstep();
+    GridRobot gridRobot= new GridRobot();
     @Test
     public void 트리플스텝테스트(){
         assertThat(thripstep.countWay(4),is(7));
+    }
+    @Test
+    public void 그리드로봇테스트(){
+        Boolean[][] maze1 = {{true,true,false},{true,true,false},{true,true,true}};
+        Boolean[][] maze2 = {{true,false,false},{false,false,false},{true,true,true}};
+        ArrayList<Point> path = new ArrayList<Point>();
+        HashSet<Point> failPoint = new HashSet<Point>();
+        assertTrue(gridRobot.getPath(maze1,2,2,path,failPoint));
+        assertFalse(gridRobot.getPath(maze2,2,2,path,failPoint));
     }
 }
